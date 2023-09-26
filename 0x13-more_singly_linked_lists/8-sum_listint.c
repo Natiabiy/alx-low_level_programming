00018975ff918a72433c678e4536755a280d284d
Natiabiy <natiabiy869@gmail.com>
2023-09-26 13:33:11 -0700

    solved

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,28 +2,21 @@
 #include <stdlib.h>
 
 /**
- * get_nodeint_at_index - function that returns the nth node of a,
+ * sum_listint - function that returns the sum of all the data (n) of a,
  * listint_t linked list.
  * @head: pointer to the head of linked list.
- * @index: index of the node, starting at 0.
  *
- * Make sure there is no memory leaks.
- *
- * Return:  and returns the nth node or NULL if node is non-existent.
+ * Return:  sum of all the data (n) of a linked list or 0 if list is empty.
  */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int sum_listint(listint_t *head)
 {
-	unsigned int i  = 0;
+	int sum = 0;
 
-    /* if there is no list return NULL */
-	if (head == NULL)
-		return (NULL);
-
-	while (i < index && head != NULL)
+	while (head != NULL)
 	{
+		sum += head->n;
 		head = head->next;
-		i++;
 	}
-	return (head);
-}
+	return (sum);
+}}
